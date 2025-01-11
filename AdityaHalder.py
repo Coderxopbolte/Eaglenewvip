732f5f9cab88fbc917a260e54fd3d55dc4d71192
Royal_Boy_Amit <116754209+royalboycoder@users.noreply.github.com>
2025-01-11 22:49:22 +0530

    Update AdityaHalder.py

--- AdityaHalder.py
+++ AdityaHalder.py
@@ -376,14 +376,33 @@ async def start_message_private(client, message):
 ├───────────────────⦿‍
 │            •••✰ 👑👑 ✰•••
 ╰───────────────────⦿"""
-        buttons = InlineKeyboardMarkup(
-            [
+            buttons = InlineKeyboardMarkup(
                 [
                     InlineKeyboardButton(
-                        text="🥀 Add Me In Your Chat ✨",
-                        url=f"https://t.me/{bot.me.username}?startgroup=true",
+                       text="𝖴ᜣ𝘥ɐꪻꫀs  ",
+                       url=f"https://t.me/VICK_NETWORK"
+                    ), 
+                    InlineKeyboardButton(
+                       text="Տꪊ𝘱Oᖇꪻ𝘴",
+                       url=f"https://t.me/VICK_SUPPORT"
                     )
                 ],
+                [
+                    InlineKeyboardButton(
+                       text="♲︎︎︎ልᴅᴅ ꪑɛ ꪮᴜʀɛ 𝖦ʀօʊप ♲︎︎",
+                       url=f"https://t.me/{bot.me.username}?startgroup=true",
+                    )
+                ],
+                [
+                    InlineKeyboardButton(text="☃︎ ꪮwꪀə᥅ ☃", 
+                       url=f"https://t.me/TEAM_EAGLE_OWNER︎", 
+                    )
+                ],
+                [
+                    InlineKeyboardButton(text="Earn Money Daily", 
+                        url=f"https://t.me/TEAM_EAGLE_AGENTS", 
+                    )
+                ] ,
                 [
                     InlineKeyboardButton(
                         text="🌺 Open Command List 🌷",
